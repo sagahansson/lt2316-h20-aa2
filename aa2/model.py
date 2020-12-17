6348e191d866e09d12daa11c1d894f759c5df499
Saga Hansson <gushansad@GU.GU.SE@mltgpu.flov.gu.se>
2020-12-17 13:14:06 +0100

    work on pt 2

---
+++ aa2/model.py
@@ -0,0 +1,18 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+class Net(nn.Module):
+    def __init__(self, in_D, hid, out_D, num_layers):
+        super(Net, self).__init__()
+        self.num_layers = num_layers
+        self.hid = hid
+        self.gru = nn.GRU(in_D, hid, num_layers, batch_first=True)
+        self.fc = nn.Linear(hid, out_D)
+    
+    def forward(self, batch, device):
+        
+        out, h_out = self.gru(batch)
+        pred = self.fc(out)
+        
+        return pred
--- aa2/trainer.py
+++ aa2/trainer.py
@@ -1,13 +1,41 @@
 
 import os
 import torch
+import torch.nn as nn
+import torch.optim as optim
+import torch.nn.functional as F
 
-
+class Batcher:
+    def __init__(self, X, y, device, batch_size=50, max_iter=None):
+        self.X = X
+        self.y = y
+        self.device = device
+        self.batch_size = batch_size
+        self.max_iter = max_iter
+        self.curr_iter = 0
+    
+    def __iter__(self):
+        return self
+    
+    def __next__(self):
+        if self.curr_iter == self.max_iter:
+            raise StopIteration
+        permutation = torch.randperm(self.X.shape[0], device=self.device)
+        permX = self.X[permutation]
+        permy = self.y[permutation]
+        splitX = torch.split(permX, self.batch_size)
+        splity = torch.split(permy, self.batch_size)
+        
+        self.curr_iter += 1
+        
+        return zip(splitX, splity)
+        
 class Trainer:
 
 
-    def __init__(self, dump_folder="/tmp/aa2_models/"):
+    def __init__(self, dump_folder="/tmp/aa2_models/", device=torch.device("cuda:3")):
         self.dump_folder = dump_folder
+        self.device = device
         os.makedirs(dump_folder, exist_ok=True)
 
 
@@ -38,13 +66,74 @@ class Trainer:
         torch.save(save_dict, os.path.join(self.dump_folder, model_name + ".pt"))
 
 
-    def load_model(self):
+    def load_model(self, path):
         # Finish this function so that it loads a model and return the appropriate variables
-        pass
+        chp = torch.load(path)
+        epoch = chp['epoch']
+        model_state_dict = chp['model_state_dict']
+        optimizer_state_dict = chp['optimizer_state_dict']
+        hyperparameters = chp['hyperparamaters']
+        loss = chp['loss']
+        scores = chp['scores']
+        model_name = chp['model_name']
+        
+        return epoch, model_state_dict, optimizer_state_dict, hyperparameters, loss, scores, model_name
 
 
-    def train(self, train_X, train_y, val_X, val_y, model_class, hyperparamaters):
+    def train(self, train_X, train_y, val_X, val_y, model_class, hyperparameters):
         # Finish this function so that it set up model then trains and saves it.
+        
+        
+        lr = hyperparameters['learning_rate']
+        n_layers = hyperparameters['number_layers']
+        epochs = hyperparameters['epochs']
+        hid_size = hyperparameters['hid_dim']
+        izer = hyperparameters['optimizer']
+        loss_function = hyperparameters['loss_funct']
+        mod_name = hyperparameters['model_name']
+        
+        device = self.device
+        
+        
+        batches = Batcher(train_X, train_y, device, max_iter=epochs)
+        
+        in_size = train_X.shape[2] # = 5 : num_features
+        out_size = 5 # jo nu är den det igen # nej nu är det något annat välkommen # num_labels, len of id2ner
+        
+        m = model_class(in_size, hid_size, out_size, n_layers)
+        m = m.to(device)
+        loss = getattr(nn, loss_function)
+        opt = getattr(optim, izer)(m.parameters(), lr=lr)
+        
+        epoch = 0
+        for batch in batches:
+            tot_loss = 0
+            for sent, labels in batch:
+                opt.zero_grad()
+                print("MODEL:", m)
+                print("SENT SHAPE", sent.shape)
+                out = m(sent.to(device), device)
+                print("OUT SHAPE", out.shape)
+                print("LABELS SHAPE", labels.shape)
+                print("LABELS", labels)
+                out = out.reshape(out.shape[0]*out.shape[1], out.shape[2])
+                print("NEW OUT SHAPE", out.shape)
+                labels = labels.reshape(labels.shape[0]*labels.shape[1], 1)
+                #out = out.reshape(out.shape[0], out.shape[2], out.shape[1]) #gör den till [50, 5, 102] ist för [50, 102, 5]
+                l = loss()(out.to(device), labels.to(device))
+                tot_loss += l
+                l.backward()
+                opt.step()
+                aosjdnoais
+            print("Total loss in epoch {} is {}".format(epoch, tot_loss))
+            epoch += 1
+        print('hello got all the way here')
+                
+        
+        
+        #loss crossent, l1
+        
+        
         pass
 
 
